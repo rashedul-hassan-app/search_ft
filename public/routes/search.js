f9129308227c0519c9b2fd1e9cfefd31485a4fd7
Rashedul Hassan <rashedul@outlook.com>
2020-03-03 00:06:01 +0000

    Formatting

--- public/routes/search.js
+++ public/routes/search.js
@@ -6,39 +6,16 @@ const path = require('path');
 var data_chunk ;
 var location = '../dummy_data.txt';
 
-function  readfile()
-{
-    
-    console.log(location);
-    
-    fs.readFile(location, 'utf8', function(err, contents) {
-        data_chunk+= contents;
-        console.log(contents.toString());
-        
-        // data_chunk = contents;
-    });
-} 
 
 
-// var call_search_api = searchlib('query');
-// console.log(call_search_api);
+
 
 /* GET search listing. */
 router.get('/', function(req, res, next) {
-    // console.log('File contents');
+
+    // const call_search_api = searchlib('query');
+    // console.log(call_search_api);
     
-    // const loadData = (location) => {
-    //     try {
-    //       return fs.readFileSync(location, 'utf8')
-    //     } catch (err) {
-    //       console.error(err)
-    //       return false
-    //     }
-    //   }
-
-    // var all_data = JSON.parse(loadData(location));
-
-    // console.log(all_data);
     var pagenumber;
     
     if (req.query.page_num == undefined)
